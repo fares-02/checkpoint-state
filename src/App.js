410c6f68100aafa5c634202f539e9944d93b1d08
fares-02 <faresbsd@gmail.com>
2022-01-19 21:44:22 +0100

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import AddItem from "./Components/AddItem/AddItem";
+import ListItems from "./Components/ListItems/ListItems";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: ["task1", "task2"],
+    };
+  }
+  handleTask = (text) => {
+    this.setState({ list: [...this.state.list, text] });
+  };
+  deleteTask = (index) => {
+    this.setState({ list: this.state.list.filter((_, i) => i !== index) });
+  };
+
+  render() {
+    return (
+      <div>
+        <AddItem handleTask={this.handleTask} />
+        <ListItems list={this.state.list} deleteTask={this.deleteTask} />
+      </div>
+    );
+  }
 }
 
 export default App;
